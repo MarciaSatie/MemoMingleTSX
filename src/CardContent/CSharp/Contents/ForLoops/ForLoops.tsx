2f4acb53431626368b74820bef263293da95689d
MarciaSatie <m.satie@gmail.com>
2023-12-02 01:21:19 +0000

    Updating C# ForLoops

--- src/CardContent/CSharp/Contents/ForLoops/ForLoops.tsx
+++ src/CardContent/CSharp/Contents/ForLoops/ForLoops.tsx
@@ -19,44 +19,167 @@ function TextContent() {
   return (
     <>
       {/* Add the New Content Bellow */}
-      <CodeMirror
-        value={`code here;`}
-        height="50%"
-        theme="dark"
-        extensions={[javascript({ jsx: true })]}
-        // onChange={onChange}
-      />
-      {/* Detail template */}
-      <div className="codeStyle">
-        <pre>
-          <code>console.log('string $&#123; variable &#125; ')</code>
-        </pre>
-      </div>
+      <details>
+        <summary>for (statement 1; statement 2; statement 3){}</summary>
+        <CodeMirror
+          value={`for (int i = 0; i < 5; i++) 
+{
+  Console.WriteLine(i);
+}`}
+          height="50%"
+          theme="dark"
+          extensions={[javascript({ jsx: true })]}
+          // onChange={onChange}
+        />
+      </details>
+      <br></br>
 
       <details>
-        <summary>Exemple</summary>
-      </details>
-      <details>
-        <summary>Explanation</summary>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-          ipsum ullam odit inventore necessitatibus accusantium quis maxime
-          dicta et harum consequuntur incidunt magni fugiat dignissimos, quam ea
-          praesentium molestias enim?
-        </p>
-      </details>
-      <details>
-        <summary>Exemple</summary>
+        <summary>Nested Loops</summary>
+        <CodeMirror
+          value={`// Outer loop
+for (int i = 1; i <= 2; ++i) 
+{
+  Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+  // Inner loop
+  for (int j = 1; j <= 3; j++) 
+  {
+    Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
+  }
+}`}
+          height="50%"
+          theme="dark"
+          extensions={[javascript({ jsx: true })]}
+          // onChange={onChange}
+        />
         <div className="codeStyle">
-          <code>bla bla bla</code>
+          <pre>
+            <code>
+              <span className="comment">//Output:</span>
+              {`
+Outer: 1
+ Inner: 1
+ Inner: 2
+ Inner: 3
+Outer: 2
+ Inner: 1
+ Inner: 2
+ Inner: 3`}
+            </code>
+          </pre>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-          ipsum ullam odit inventore necessitatibus accusantium quis maxime
-          dicta et harum consequuntur incidunt magni fugiat dignissimos, quam ea
-          praesentium molestias enim?
-        </p>
       </details>
+      <br></br>
+
+      <details>
+        <summary>foreach (type variableName in arrayName){}</summary>
+        <p>Just to loop Arrays</p>
+        <CodeMirror
+          value={`string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+foreach (string i in cars) 
+{
+  Console.WriteLine(i);
+} ;
+//Volvo, BMW, Ford, Mazda`}
+          height="50%"
+          theme="dark"
+          extensions={[javascript({ jsx: true })]}
+          // onChange={onChange}
+        />
+      </details>
+      <br></br>
+
+      <details>
+        <summary>Adding Break</summary>
+        <p>
+          The break statement can also be used to jump out of a loop.<br></br>
+          This example jumps out of the loop when i is equal to 4:
+        </p>
+        <CodeMirror
+          value={`for (int i = 0; i < 10; i++) 
+{
+  if (i == 4) 
+  {
+    Console.WriteLine("stopping for loop");
+    break;
+  }
+  Console.WriteLine(i);
+}`}
+          height="50%"
+          theme="dark"
+          extensions={[javascript({ jsx: true })]}
+          // onChange={onChange}
+        />
+        <div className="codeStyle">
+          <pre>
+            <code>
+              <span className="comment">//Output:</span>
+              {`
+0
+1
+2
+3
+stopping for loop`}
+            </code>
+          </pre>
+        </div>
+      </details>
+      <br></br>
+
+      <details>
+        <summary>Adding Continue</summary>
+        <p>
+          The continue statement breaks one iteration (in the loop), if a
+          specified condition occurs, and continues with the next iteration in
+          the loop.<br></br>
+          TThis example skips the value of 4:
+        </p>
+        <CodeMirror
+          value={`using System;
+          ​
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) 
+        {//run the code inside of this if stetament and continue the loop
+          continue;
+        }
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}`}
+          height="50%"
+          theme="dark"
+          extensions={[javascript({ jsx: true })]}
+          // onChange={onChange}
+        />
+        <div className="codeStyle">
+          <pre>
+            <code>
+              <span className="comment">//Output:</span>
+              {`
+0
+1
+2
+3
+5
+6
+7
+8
+9`}
+            </code>
+          </pre>
+        </div>
+      </details>
+      <br></br>
     </>
   );
 }
