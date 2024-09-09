b69f2c13a08aaf631780f40ffa2536c64739201f
MarciaSatie <m.satie@gmail.com>
2024-09-10 00:29:31 +0100

    update memo app

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ function App() {
 
   return (
     <>
+      <h1 className="text-3xl text-red-600 font-bold underline">
+      Hello world!
+      </h1>
       <Header />
 
       <CardGroupSearch list={listAll} />
